1342b944ab20488224d8294454a6781971b88e2a
julioram151005 <jramirezp41@miumg.edu.gt>
2025-08-22 01:32:34 -0600

    Errores Colegidos

---
+++ server.js
@@ -0,0 +1,36 @@
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const cors = require("cors");
+  const app = express();
+
+  const corsOptions = {
+    origin: "http://localhost:8081" 
+  };
+  
+  app.use(cors(corsOptions));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+
+    const db = require("./API_UNIVERSIDAD/models");
+
+  db.sequelize.sync()
+    .then(() => {
+      console.log("Base de datos sincronizada.");
+    })
+    .catch(err => {
+      console.error("Error al sincronizar la base de datos:", err.message);
+    });
+
+
+  require("./API_UNIVERSIDAD/routes/student.route.js")(app);
+
+  app.get("/", (req, res) => {
+    res.json({ message: "Bienvenido a la API de la UMG." });
+  });
+
+  const PORT = process.env.PORT || 8081;
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}.`);
+  });
+    
