74967b10db5707a5a6c22960ea14b1a79d8ca8e6
julioram151005 <jramirezp41@miumg.edu.gt>
2025-09-05 13:07:45 -0600

    CAmbio de tabla estudiantes dentro de Grado.java

--- JavaProyect-main/src/main/java/com/mycompany/sistemaintecap/Grado.java
+++ JavaProyect-main/src/main/java/com/mycompany/sistemaintecap/Grado.java
@@ -49,17 +49,17 @@ public class Grado extends javax.swing.JFrame {
         jTable1.setForeground(new java.awt.Color(255, 255, 255));
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null}
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
             },
             new String [] {
-                "ID", "CARNET", "NOMBRE", "EMAIL", "CURSO", "ESPECIALIDAD", "HORARIO"
+                "ID", "NOMBRE", "EMAIL", "CARNET", "ESTADO"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.Object.class, java.lang.String.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
 
             public Class getColumnClass(int columnIndex) {
