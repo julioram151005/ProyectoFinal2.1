9f3ed248fa82e16ca738b9b2aa0017d6430fdd5c
julioram151005 <jramirezp41@miumg.edu.gt>
2025-09-07 13:23:40 -0600

    Añadi especialidad

--- API_UNIVERSIDAD/models/asignacion.model.js
+++ API_UNIVERSIDAD/models/asignacion.model.js
@@ -19,6 +19,9 @@ module.exports = (sequelize, Sequelize) => {
                 key: 'id'
             }
         },
+        especialidad: {
+            type: Sequelize.STRING
+        },
         modalidad: {
             type: Sequelize.STRING
         },
